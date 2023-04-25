8ffb2a649edc4897b029094247ebd2c99f38733f
Andrés Pata Fraile de Manterola <apfm21@Gmail.com>
2023-04-24 22:23:32 -0300

    microdesafio 0 y 1

---
+++ Clase 1/micro1/micro1.js
@@ -0,0 +1,24 @@
+// Saludo al usuario
+
+// const nombre = prompt("Escriba su nombre")
+
+// // const procesarNombre1 = "Hola " + nombre + " bienvenido al curso de Js"
+
+// const procesarNombre2 = `Hola ${nombre} bienvenido al curso de Js`
+
+// console.log(procesarNombre2)
+
+
+// calculadora que solo multiplica
+// num1*num2 = num3
+
+// pedir numeros
+const num1 = Number(prompt("ingrese 'num1' de: \n num1 * num2 = num3 \n y nosotros les devolvemos num3"))
+const num2 = Number(prompt("ingrese 'num1' de: \n num1 * num2 = num3 \n y nosotros les devolvemos num3"))
+
+// procesar número
+const num3 = num1*num2
+
+// devolver resultado
+console.log(`num1(${num1}) multiplicado por num2(${num2}) es igual a ${num3}`)
+
