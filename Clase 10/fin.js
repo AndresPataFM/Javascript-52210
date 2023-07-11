00194d15ef87856878c06cf83148375a40fc66b8
Andrés Pata Fraile de Manterola <apfm21@Gmail.com>
2023-07-10 22:02:45 -0300

    clase 10

---
+++ Clase 10/fin.js
@@ -0,0 +1,48 @@
+// Que es node?
+// En bruto, node es un motor de Js que se encuentra en la computadora en vez del ordenador
+
+// Express
+// Un framework de Node
+
+// Stacks
+
+// MERN
+// MongoDB, Express, React y Node
+
+// MEAN
+// MongoDB, Express, Angular y Node
+
+// MEVN
+// MongoDB, Express, Vue y Node
+
+// Flutter
+
+// LAMP
+// Linux, Apache, MySQL y PHP
+
+// Ruby on Rails (RoR0)
+// Ruby con el framework Rails
+
+// Lenguages/Frameworks/Etc que me parecen interesantes de aprender ademas de lo visto:
+
+// Java (Spring)
+// C# (.NET)
+// Python (Django)
+
+
+// Typescript
+// Node
+
+// React (vean avanzado además de básico, cosas como crear )
+// Angular
+
+// MySQL, SQLite, Postgres (Bases de dato SQL)
+//  MongoDB, Cassandra, Apache storm, Sphinx (Bases de Dato no SQL)
+
+// Testeo (cosas como Junit/Jest/NUnit/Xunit/Pytest)
+
+// HAGAN PROYECTOSSSSSS
+
+console.log("hola")
+console.table([1,2,3,4,5])
+prompt()
